a0b946c50c3813302a8ef4768ae18d04e356a463
pwnzGr3y <pwnzgr3y@yandex.ru>
2025-06-28 20:30:33 +0300

    настройка веб-пака

---
+++ src/scripts/index.js
@@ -0,0 +1,33 @@
+import "../pages/index.css";
+import { initialCards } from "../components/cards";
+
+// @todo: Темплейт карточки
+
+const cardTemplate = document.querySelector('#card-template').content;
+// @todo: DOM узлы
+
+const cardList = document.querySelector('.places__list');
+// @todo: Функция создания карточки
+
+const createCard = (cardData, deleteCard) => {
+    const card = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardTitle = card.querySelector('.card__title');
+    const cardImage = card.querySelector('.card__image');
+    const cardDeleteButton = card.querySelector('.card__delete-button');
+
+    cardTitle.textContent = cardData.name;
+    cardImage.src = cardData.link;
+    cardImage.alt = `Фото места ${cardData.name}`;
+    cardDeleteButton.addEventListener('click', deleteCard);
+
+    return card;
+};
+// @todo: Функция удаления карточки
+const deleteCard = (evt) => {
+    evt.target.closest('.card').remove();
+};
+// @todo: Вывести карточки на страницу
+initialCards.forEach((data) => {
+    const cardElement = createCard(data, deleteCard);
+    cardList.append(cardElement);
+});
