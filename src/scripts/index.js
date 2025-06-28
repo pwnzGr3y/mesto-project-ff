2c1d6372241ef6a77c2689c795f01a90fc9a226d
pwnzGr3y <pwnzgr3y@yandex.ru>
2025-06-28 20:39:02 +0300

    настройка линта

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,33 +1,33 @@
-import "../pages/index.css";
-import { initialCards } from "../components/cards";
+import { initialCards } from '../components/cards'
+import '../pages/index.css'
 
 // @todo: Темплейт карточки
 
-const cardTemplate = document.querySelector('#card-template').content;
+const cardTemplate = document.querySelector('#card-template').content
 // @todo: DOM узлы
 
-const cardList = document.querySelector('.places__list');
+const cardList = document.querySelector('.places__list')
 // @todo: Функция создания карточки
 
-const createCard = (cardData, deleteCard) => {
-    const card = cardTemplate.querySelector('.card').cloneNode(true);
-    const cardTitle = card.querySelector('.card__title');
-    const cardImage = card.querySelector('.card__image');
-    const cardDeleteButton = card.querySelector('.card__delete-button');
+function createCard(cardData, deleteCard) {
+  const card = cardTemplate.querySelector('.card').cloneNode(true)
+  const cardTitle = card.querySelector('.card__title')
+  const cardImage = card.querySelector('.card__image')
+  const cardDeleteButton = card.querySelector('.card__delete-button')
 
-    cardTitle.textContent = cardData.name;
-    cardImage.src = cardData.link;
-    cardImage.alt = `Фото места ${cardData.name}`;
-    cardDeleteButton.addEventListener('click', deleteCard);
+  cardTitle.textContent = cardData.name
+  cardImage.src = cardData.link
+  cardImage.alt = `Фото места ${cardData.name}`
+  cardDeleteButton.addEventListener('click', deleteCard)
 
-    return card;
-};
+  return card
+}
 // @todo: Функция удаления карточки
-const deleteCard = (evt) => {
-    evt.target.closest('.card').remove();
-};
+function deleteCard(evt) {
+  evt.target.closest('.card').remove()
+}
 // @todo: Вывести карточки на страницу
 initialCards.forEach((data) => {
-    const cardElement = createCard(data, deleteCard);
-    cardList.append(cardElement);
-});
+  const cardElement = createCard(data, deleteCard)
+  cardList.append(cardElement)
+})
